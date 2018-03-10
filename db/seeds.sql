683dfdb50bbde1a73a70c3df74f28cb8f2fb936b
davidl199 <davidl1_99@yahoo.com>
2018-03-10 09:57:11 -0500

    Initial commit for directory structure and database orm setup

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Cheesburger Avacado');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
