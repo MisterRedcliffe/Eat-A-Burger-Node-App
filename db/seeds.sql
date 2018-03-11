815e8678eecdcad4ca883af6bae0c8733f0ee90e
davidl199 <davidl1_99@yahoo.com>
2018-03-11 19:21:18 -0400

    Updated controller,routes and index.handlebars files

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,4 @@ USE burgers_db;
 INSERT INTO burgers (burger_name) VALUES ('Cheesburger Avacado');
 INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
 INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Cheap Burger',1);
--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,6 @@
 var orm = require("../config/orm.js");
 
-var buger = {
+var burger = {
   selectAll: function(cb) {
     orm.selectAll("burgers", function(res) {
       cb(res);
